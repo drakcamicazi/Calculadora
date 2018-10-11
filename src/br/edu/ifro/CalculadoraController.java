1d59045ab4e0fdcfccdc7fdedd88b97e7f8eb931
Milan <drak.milena@gmail.com>
2018-10-11 15:58:09 -0400

    Calculadora pronta, falta autenticação

--- src/br/edu/ifro/CalculadoraController.java
+++ src/br/edu/ifro/CalculadoraController.java
@@ -6,13 +6,23 @@
 package br.edu.ifro;
 
 import java.net.URL;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 /**
  * FXML Controller class
@@ -21,6 +31,8 @@ import javafx.scene.text.Text;
  */
 public class CalculadoraController implements Initializable {
 
+    @FXML
+    private TableView tvHistorico;
     @FXML
     private TextField txtValor1;
     @FXML
@@ -36,30 +48,85 @@ public class CalculadoraController implements Initializable {
     @FXML
     private Text txtResultado;
 
+    private String operando;
+    private Double resultado;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        atualizarTabela();
+    }
 
     @FXML
     private void Somar(ActionEvent event) {
+        operando = "+";
+        resultado = Double.parseDouble(txtValor1.getText()) + Double.parseDouble(txtValor2.getText());
+        txtResultado.setText(resultado.toString());
+        adicionarRegistro();
+        limpar();
+        atualizarTabela();
     }
 
     @FXML
     private void Subtrair(ActionEvent event) {
+        operando = "-";
+        resultado = Double.parseDouble(txtValor1.getText()) - Double.parseDouble(txtValor2.getText());
+        txtResultado.setText(resultado.toString());
+        adicionarRegistro();
+        limpar();
+        atualizarTabela();
     }
 
     @FXML
     private void Multiplicar(ActionEvent event) {
+        operando = "*";
+        resultado = Double.parseDouble(txtValor1.getText()) * Double.parseDouble(txtValor2.getText());
+        txtResultado.setText(resultado.toString());
+        adicionarRegistro();
+        limpar();
+        atualizarTabela();
     }
 
     @FXML
     private void Dividir(ActionEvent event) {
+        operando = "/";
+        resultado = Double.parseDouble(txtValor1.getText()) / Double.parseDouble(txtValor2.getText());
+        txtResultado.setText(resultado.toString());
+        adicionarRegistro();
+        limpar();
+        atualizarTabela();
     }
-    
-    private void limpar(){
+
+    private void limpar() {
         txtValor1.setText("");
         txtValor2.setText("");
-        txtResultado.setText("");
+    }
+
+    private void adicionarRegistro() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("calculadora");
+        EntityManager em = emf.createEntityManager();
+
+        Historico historico = new Historico();
+        historico.setOperador(operando);
+        historico.setPrimeiroValor(Double.parseDouble(txtValor1.getText()));
+        historico.setSegundoValor(Double.parseDouble(txtValor2.getText()));
+        historico.setResultado(resultado);
+
+        em.getTransaction().begin();
+        em.persist(historico);
+        em.getTransaction().commit();
+
+        em.close();
+        emf.close();
+    }
+
+    private void atualizarTabela() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("calculadora");
+        EntityManager em = emf.createEntityManager();
+        Query query = em.createQuery("SELECT h FROM Historico as h");
+        List<Historico> h = query.getResultList();
+        ObservableList oh = FXCollections.observableArrayList(h);
+        tvHistorico.setItems(oh);
+        em.close();
+        emf.close();
     }
 }
--- src/br/edu/ifro/Historico.java
+++ src/br/edu/ifro/Historico.java
@@ -1,12 +1,13 @@
 package br.edu.ifro;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Historico {
+public class Historico implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
